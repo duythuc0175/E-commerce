8c7174b0e4b0f233afe5b7dcb97f25968a0ed5a0
XuanSon213a <xuanson2002n@gmail.com>
2023-12-11 22:27:44 +0700

    change logoheader

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,8 +1,8 @@
-import logoDark from "./logoDark.png";
+import logo from "./logo.jpg";
 import logoLight from "./logoLight.png";
 import cartImg from "./cart.png";
 import paymentLogo from "./paymentLogo.png";
 import googleLogo from "./googleLogo.png";
 import githubLogo from "./githubLogo.png";
 
-export { logoDark, logoLight, cartImg, paymentLogo, googleLogo, githubLogo };
+export { logo, logoLight, cartImg, paymentLogo, googleLogo, githubLogo };
