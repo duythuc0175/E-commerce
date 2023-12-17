207ea6f9a404240296b3b4fe55ac088df7e5efbf
Pham <minh.tran.ext@lvmh.com>
2023-12-17 22:05:11 +0700

    minor interface updates

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -17,7 +17,7 @@ const CartItem = () => {
   return (
     <div className="w-2/3 pr-10">
       <div className="w-full">
-        <h2 className="font-titleFont text-2xl">shopping cart</h2>
+        <h2 className="font-titleFont text-2xl">Shopping Cart</h2>
         <div>
           <div>
             {productData.map((item) => (
@@ -100,7 +100,7 @@ const CartItem = () => {
           <span>
             <HiOutlineArrowLeft />
           </span>
-          go shopping
+          Go shopping
         </button>
       </Link>
       <ToastContainer
